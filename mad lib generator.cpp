6288ea9a2fe4b6b571169704d5abf343d03271aa
ofarukdemirtas <omrfarukdemir1@gmail.com>
2021-07-24 21:11:10 +0300

    First commit

---
+++ mad lib generator.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+char a[20];
+char b[20] ;
+char c[20] ;
+char d[20] ;
+char e[20] ;
+char f[20] ;
+char g[20] ;
+char h[20] ;
+char i[20] ;
+char j[10] ;
+char k[10] ;
+char l[10] ;
+char m[20] ;
+char n[20] ;
+char o[20] ;
+char p[20] ;
+char q[20] ;
+
+printf("Enter one of your friend's name: ");
+scanf("%s",&a);
+ 
+printf("\nNoun: ");
+ scanf("%s",&b);
+
+printf("\nSuperlative ends with 'est': ");
+ scanf("%s",&c);
+
+printf("\nNoun: ");
+ scanf("%s",&d);
+
+printf("\nBODY PART: ");
+ scanf("%s",&e);
+
+printf("\nVerb endin 'ing': ");
+ scanf("%s",&f);
+
+printf("\nNoun: ");
+ scanf("%s",&g);
+
+printf("\nVerb: ");
+ scanf("%s",&h);
+
+printf("\nEvent: ");
+ scanf("%s",&i);
+
+printf("\nChoose a day: ");
+ scanf("%s",&j);
+
+printf("\nVerb: ");
+ scanf("%s",&k);
+
+printf("\nVerb: ");
+ scanf("%s",&l);
+
+printf("\nPlace: ");
+ scanf("%s",&m);
+
+printf("\nTime span: ");
+ scanf("%s",&n);
+
+printf("\nVerb: ");
+ scanf("%s",&o);
+
+printf("\nAdverb: ");
+ scanf("%s",&p);
+
+printf("\nEnter one of your friend's name: ");
+ scanf("%s",&q);
+ 
+printf("\nDear %s,\n",a);
+printf(" It has come to my %s that you are the %s girl/boy in the %s.\n",b,c,d);
+printf(" My %s starts %s a %s every time you speak.\n ",e,f,g);
+printf("I would like to %s if you want to go to the %s with me next %s.\n",h,i,j);
+printf(" If you %s please %s me at the %s in %s. I %s you and everything about you.\n",k,l,m,n,o);
+ 
+printf(" %s, %s \n ",p,q);
+
+  }
+  
+  
+  
+  
+  
+  
+ 
